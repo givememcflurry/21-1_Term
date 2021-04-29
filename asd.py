d2adcc5858de49c35a8881857d76a09a73eccd11
givememcflurry <pksildung@naver.com>
2021-04-30 00:56:22 +0900

    - Add BW & Resonant freq together calculation - bnr_test shows all of calculation result - Add Line break for visibility

--- asd.py
+++ asd.py
@@ -1,11 +1,11 @@
-# 2021 1학기 텀프로젝트 소자값계산툴
-import math
+# 2021-1 Device-Value Calculate Module :)
+import math as mt
 
 res_list = [0.001, 0.0022, 0.0033, 0.0047, 0.0056, 0.0068, 0.0075, 0.0091, 0.01, 0.022, 0.033, 0.051, 0.082, 0.1, 0.22,
             0.33, 0.47, 0.56, 0.68, 0.82, 1]  # kilo Ohm
-ind_list = [0.22, 0.33, 0.47, 0.56, 0.68, 0.82, 1, 1.2, 1.5, 2.7, 3.3, 4.7, 10, 22, 33, 68]  # mili Henri
+ind_list = [0.22, 0.33, 0.47, 0.56, 0.68, 0.82, 1, 1.2, 1.5, 2.7, 3.3, 4.7, 10, 22, 33, 68]  # milli Henri
 cap_list = [0.033, 0.047, 0.068, 0.1, 0.12, 0.15, 0.18, 0.22, 0.33, 0.47, 0.56, 0.68, 1, 2.2, 3.3, 4.7, 5.6, 6.8, 10,
-            22, 33, 47, 100, 220, 330, 470, 560]  # micro Ferad
+            22, 33, 47, 100, 220, 330, 470, 560]  # micro Farad
 
 
 def bbc(res, ind):  # BPF BW Calculate
@@ -16,16 +16,16 @@ def bbc(res, ind):  # BPF BW Calculate
     return bwf
 
 
-def rfc(ind, cap): # Resonant Freq. Calculate
+def rfc(ind, cap):  # Resonant Freq. Calculate
     input_ind = ind * 1e-3
     input_cap = cap * 1e-6
     pi = 3.141592
-    rf = 1 / (2 * pi * math.sqrt(input_ind*input_cap))
-    return rf
+    rsf = 1 / (2 * pi * mt.sqrt(input_ind * input_cap))
+    return rsf
 
 
 def bbd():  # BPF BW Device-value calculate
-    fb = 2000
+    fb = 2000  # Band Width Feq = 2kHz
     dif = 9999
     res_num = len(res_list)
     ind_num = len(ind_list)
@@ -46,6 +46,80 @@ def bbd():  # BPF BW Device-value calculate
     print("BW = ", round(bbc(res_save, ind_save)))
 
 
+def per(std_freq, chk_freq):  # Difference Percentage
+    per_dif = abs(std_freq - chk_freq) / std_freq * 100
+    return per_dif
+
+
+def bnr(input_fb, input_fr):  # ★find device-value from BW & Resonant freq.★
+    fb = input_fb  # Band Width Feq = 2kHz
+    fr = input_fr  # Resonant Freq. = 3kHz
+    res_num = len(res_list)
+    ind_num = len(ind_list)
+    cap_num = len(cap_list)
+    rc, ic, cc = 0, 0, 0
+    res_save, ind_save, cap_save = 0, 0, 0
+    dif = 1000
+    while rc < res_num:
+        while ic < ind_num:
+            while cc < cap_num:
+                bbd_bwf = bbc(res_list[rc], ind_list[ic])
+                rfc_rsf = rfc(ind_list[ic], cap_list[cc])
+
+                per_bwf = per(fb, bbd_bwf)
+                per_ref = per(fr, rfc_rsf)
+
+                cal_per = mt.pow(per_bwf, 2) + mt.pow(per_ref, 2)
+
+                if abs(cal_per) < dif:
+                    dif = cal_per
+                    res_save = res_list[rc]
+                    ind_save = ind_list[ic]
+                    cap_save = cap_list[cc]
+
+                cc = cc + 1
+            ic = ic + 1
+            cc = 0
+        rc = rc + 1
+        ic = 0
+
+    print("R = ", res_save, "kOhm")
+    print("L = ", ind_save, "mH")
+    print("C = ", cap_save, "uF")
+    print("BW = ", bbc(res_save, ind_save), " RF = ", rfc(ind_save, cap_save))
+
+
+def bnr_test():  # ★find device-value from BW & Resonant freq.★
+    fb = 2000  # Band Width Feq = 2kHz
+    fr = 3000  # Resonant Freq. = 3kHz
+    res_num = len(res_list)
+    ind_num = len(ind_list)
+    cap_num = len(cap_list)
+    rc, ic, cc = 0, 0, 0
+    while rc < res_num:
+        while ic < ind_num:
+            while cc < cap_num:
+                bbd_bwf = bbc(res_list[rc], ind_list[ic])
+                rfc_rsf = rfc(ind_list[ic], cap_list[cc])
+
+                per_bwf = per(fb, bbd_bwf)
+                per_ref = per(fr, rfc_rsf)
+
+                cal_per = mt.pow(per_bwf, 2) + mt.pow(per_ref, 2)
+
+                print("R = ", res_list[rc], "kOhm")
+                print("L = ", ind_list[ic], "mH")
+                print("C = ", cap_list[cc], "uF")
+                print("BW = ", bbc(res_list[rc], ind_list[ic]), " RF = ", rfc(ind_list[ic], cap_list[cc]))
+                print("Per = ", cal_per)
+
+                cc = cc + 1
+            ic = ic + 1
+            cc = 0
+        rc = rc + 1
+        ic = 0
+
+
 def lcc(res, cap):  # LPF Cutoff-Freq. Calculate
     input_res = res * 1e+3
     input_cap = cap * 1e-6
@@ -54,7 +128,7 @@ def lcc(res, cap):  # LPF Cutoff-Freq. Calculate
     return lfc
 
 
-def frc(input_freq):  # Find Res, Cap - LPF
+def frc(input_freq):  # ★Find Res, Cap - LPF★
     fcl = input_freq
     dif = 1e+5
     res_num = len(res_list)
@@ -64,10 +138,12 @@ def frc(input_freq):  # Find Res, Cap - LPF
     while rc < res_num:
         while cc < cap_num:
             lcc_lfc = lcc(res_list[rc], cap_list[cc])
+
             if abs(lcc_lfc - fcl) < dif:
                 dif = abs(lcc_lfc - fcl)
                 res_save = res_list[rc]
                 cap_save = cap_list[cc]
+
             cc = cc + 1
         rc = rc + 1
         cc = 0
@@ -92,7 +168,7 @@ def hcc(res, ind):  # HPF Cutoff-Freq. Calculate
     return hfc
 
 
-def fri(input_freq):  # Find Res, Ind - HPF
+def fri(input_freq):  # ★Find Res, Ind - HPF★
     fch = input_freq
     dif = 1e+5
     res_num = len(res_list)
@@ -102,10 +178,12 @@ def fri(input_freq):  # Find Res, Ind - HPF
     while rc < res_num:
         while ic < ind_num:
             hcc_hfc = hcc(res_list[rc], ind_list[ic])
+
             if abs(hcc_hfc - fch) < dif:
                 dif = abs(hcc_hfc - fch)
                 res_save = res_list[rc]
                 ind_save = ind_list[ic]
+
             ic = ic + 1
         rc = rc + 1
         ic = 0
